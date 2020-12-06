7fdacf827d1944c73540443bcce5fb8e7f123b01
Luca Cortese <cortese@workup.it>
2020-12-06 19:07:09 +0100

    Added DB Config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	// variabili configurazione connessione al database
-	$servername = "localhost";
-	$username = "test";
-	$password = "CCmi29dk1opa0_";
-	$dbname = "food";
+	$servername = "us-cdbr-east-02.cleardb.com";
+	$username = "b8f7d0e56a9e60";
+	$password = "96ca32c9";
+	$dbname = "heroku_500be0e37a78e96";
